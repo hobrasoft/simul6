e9c92724e0df06cca94482f5546057616dd8f605
Petr Bravenec <petr.bravenec@hobrasoft.cz>
2019-07-13 22:26:31 +0200

    Opravený current/previous řádek při výběru z databáze

--- databaseview.cpp
+++ databaseview.cpp
@@ -5,6 +5,7 @@ DatabaseView::DatabaseView(QWidget *parent) : QTreeView(parent)
 }
 
 void DatabaseView::currentChanged(const QModelIndex& current, const QModelIndex& previous) {
-    int row = previous.row();
+    Q_UNUSED(previous);
+    int row = current.row();
     emit currentRowChanged(row);
 }
