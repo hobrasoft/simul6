4118f15ba1fe942bbe4676dca9a5b6d1963e2f5b
Petr Bravenec <petr@bravenec.org>
2019-08-27 16:43:41 +0200

    Vyhozené úvodní série v grafu

--- graf.cpp
+++ graf.cpp
@@ -8,36 +8,9 @@
 Graf::Graf(QWidget *parent) : QChartView(parent)
 {
     setRenderHint(QPainter::Antialiasing);
-    QLineSeries *series1 = new QLineSeries(this);
-    series1->setName("Kuk");
-    *series1
-        << QPointF(2, 4)
-        << QPointF(4, 1)
-        << QPointF(6, 2)
-        << QPointF(8, 7)
-        << QPointF(10, 20)
-        << QPointF(12, 4)
-        << QPointF(14, 7)
-        << QPointF(16, 2)
-        << QPointF(20, 9);
-
-    QLineSeries *series2 = new QLineSeries(this);
-    for (double x=2; x<25; x +=0.01){
-        double y = sin(x)*3 + 10;
-        *series2 << QPointF(x, y);
-    }
-
-
     QChart *chart = new QChart();
-
-    //chart->legend()->hide();
-    chart->addSeries(series1);
-    chart->addSeries(series2);
     chart->createDefaultAxes();
-    chart->setTitle(tr("Line Chart Graph"));
-
     setChart(chart);
-
 }
 
 void Graf::drawGraph(Engine *pEngine, std::vector<double> *pHpl)
