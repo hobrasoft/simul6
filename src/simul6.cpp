4abf38a0a9de84eb8d0a1385da9b91e68cc9848b
Petr Bravenec <petr@bravenec.org>
2020-07-31 08:44:28 +0200

    Fixed SIGSEGV after CSV saving

--- src/simul6.cpp
+++ src/simul6.cpp
@@ -43,8 +43,8 @@ Simul6::Simul6(QWidget *parent) :
     ui->setupUi(this);
     setWindowIcon(QIcon("://icons/appicon.svg"));
     createActions();
-    connect(ui->f_computeControl, &ComputeControl::init, this, &Simul6::initEngine);
     connect(ui->f_computeControl, &ComputeControl::init, SAVEPROGRESS, &SaveProgress::init);
+    connect(ui->f_computeControl, &ComputeControl::init, this, &Simul6::initEngine);
     connect(ui->f_computeControl, &ComputeControl::init, ui->f_mixcontrol, &MixControl::init);
     connect(ui->f_computeControl, &ComputeControl::init, ui->f_simulationProfile, &SimulationProfile::init);
     connect(ui->f_computeControl, &ComputeControl::run, this, &Simul6::runEngine);
