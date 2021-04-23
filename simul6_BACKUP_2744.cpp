1820cbeab89f729ce6f72ecd0ab0f2d0d90065e0
Bohuslav Gas <bohuslav.gas@gmail.com>
2021-04-23 13:15:33 +0200

    version

---
+++ simul6_BACKUP_2744.cpp
@@ -0,0 +1,122 @@
+#include "simul6.h"
+#include "preferences.h"
+#include "ui_simul6.h"
+#include "computecontrol.h"
+#include "simulationprofile.h"
+#include "constituentsdialog.h"
+#include "msettings.h"
+#include "messagedialog.h"
+#include <QSize>
+#include <QMessageBox>
+#include <QTimer>
+
+Simul6::Simul6(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Simul6)
+{
+    MSettings::instance(this);
+    ui->setupUi(this);
+    createActions();
+<<<<<<< HEAD
+    connect(ui->f_computeControl, &ComputeControl::init, ui->f_simulationProfile, &SimulationProfile::slotInit);
+    connect(ui->f_computeControl, &ComputeControl::run, [this]() {
+        ui->f_parameters->setEnabled(false);
+        //double current = ui->f_parameters->current();
+        double dt = ui->f_parameters->dt();
+        double Voltage = ui->f_parameters->Voltage();
+        bool optimizeDt = ui->f_parameters->optimizeDt();
+        ui->f_simulationProfile->engine()->setVoltage(Voltage);
+        ui->f_simulationProfile->engine()->setDt(dt);
+        ui->f_simulationProfile->engine()->setOptimizeDt(optimizeDt);
+        ui->f_simulationProfile->slotRun();
+    });
+    connect(ui->f_computeControl, &ComputeControl::stop, [this]() {
+        ui->f_parameters->setEnabled(true);
+        ui->f_simulationProfile->slotStop();
+    });
+=======
+    connect(ui->f_computeControl, &ComputeControl::init, this, &Simul6::initEngine);
+    connect(ui->f_computeControl, &ComputeControl::run, this, &Simul6::runEngine);
+    connect(ui->f_computeControl, &ComputeControl::stop, this, &Simul6::stopEngine);
+
+>>>>>>> 110811c0b3d7593aeb179a379bde2b95098fe7de
+    connect(ui->f_simulationProfile, &SimulationProfile::timeChanged, ui->f_computeControl, &ComputeControl::showTime);
+
+    connect(ui->f_simulationProfile, &SimulationProfile::finished, this, &Simul6::engineFinished);
+    QTimer::singleShot(0, this, &Simul6::init);
+}
+
+
+void Simul6::init() {
+}
+
+    
+void Simul6::engineFinished() {
+    ui->f_parameters->setEnabled(true);
+    ui->f_computeControl->slotFinished();
+
+}
+
+void Simul6::runEngine() {
+    ui->f_parameters->setEnabled(false);
+    double current = ui->f_parameters->current();
+    double dt = ui->f_parameters->dt();
+    bool optimizeDt = ui->f_parameters->optimizeDt();
+    ui->f_simulationProfile->engine()->setCurDen(-508*current);
+    ui->f_simulationProfile->engine()->setDt(dt);
+    ui->f_simulationProfile->engine()->setOptimizeDt(optimizeDt);
+    ui->f_simulationProfile->slotRun();
+}
+
+
+void Simul6::initEngine() {
+    ui->f_simulationProfile->engine()->init();
+}
+
+void Simul6::stopEngine() {
+    ui->f_parameters->setEnabled(true);
+    ui->f_simulationProfile->slotStop();
+}
+
+
+void Simul6::createActions() {
+    QMenu *menu = new QMenu(tr("Application"), this);
+    QAction *action;
+    action = new QAction(tr("Quit application"), this);
+    menu->addAction(action);
+    connect(action, &QAction::triggered, [this]() {
+        zavrit();
+    });
+
+    action = new QAction(tr("Preferences"), this);
+    connect(action, &QAction::triggered, [this]() {
+        Preferences preferences(this);
+        preferences.exec();
+    });
+    menu->addAction(action);
+
+    ui->menuBar->addMenu(menu);
+}
+
+
+void Simul6::zavrit() {
+    int rc= QMessageBox::question(this,
+            tr("I have a question"),
+            tr("Do you really want to close the application?"),
+            QMessageBox::Yes, QMessageBox::No, QMessageBox::NoButton);
+    if (rc == QMessageBox::Yes) {
+        QCoreApplication::quit();
+    }
+}
+
+
+void Simul6::closeEvent(QCloseEvent *event) {
+    zavrit();
+    event->ignore();
+}
+
+
+Simul6::~Simul6()
+{
+    delete ui;
+}
---
+++ src/graf_BASE_2296.cpp
@@ -0,0 +1,135 @@
+#include "graf.h"
+#include "omp.h"
+#include "pdebug.h"
+#include "constituentseries.h"
+#include <iostream>
+#include <cmath>
+#include <QValueAxis>
+
+Graf::Graf(QWidget *parent) : QChartView(parent)
+{
+    setRenderHint(QPainter::Antialiasing);
+    setRubberBand(QChartView::RectangleRubberBand);
+    m_chart = new QChart();
+    setChart(m_chart);
+
+    m_db = nullptr;
+    // m_db = new Db::Database("abcd.simul6.sqlite3", this);
+}
+
+
+void Graf::mouseReleaseEvent(QMouseEvent *event) { 
+    if (event->button() != Qt::RightButton) { 
+        QChartView::mouseReleaseEvent(event);
+        return; 
+        }
+    m_chart->zoomReset();
+    event->accept();
+}
+
+
+void Graf::init(const Engine *pEngine) {
+    double maximum = -99999;
+    m_chart->removeAllSeries();
+    pEngine->lock();
+    size_t p = pEngine->getNp(); // points
+    double inc_x = pEngine->getCapLen() / p;
+    int id = 0;
+    for (auto &sample : pEngine->getMix().getSamples()) {
+        QLineSeries *series = new ConstituentSeries(sample, this);
+
+        m_chart->addSeries(series);
+        double x = 0;
+        for (unsigned int i = 0; i <= p; i++){
+            series->append(QPointF(x * 1000.0, sample.getA(0, i)));
+            if (sample.getA(0, i) > maximum)  {
+                maximum = sample.getA(0,i);
+                }
+            x += inc_x;
+            }
+        id += 1;
+        }
+
+    QLineSeries *series = new QLineSeries(this);
+    series->setName(tr("pH"));
+    series->setUseOpenGL(true);
+    double x = 0;
+    auto hpl = pEngine->getHpl();
+    for (unsigned int i = 0; i <= p; i++){
+        if (hpl[i] > 0) {
+            series->append(QPointF(x * 1000.0, -log(hpl[i]) / log(10)));
+            }
+        x += inc_x;
+        }
+    pEngine->unlock();
+
+    m_chart->addSeries(series);
+    m_chart->legend()->setVisible(false);
+    m_chart->createDefaultAxes();
+    QList<QAbstractAxis *> axes = m_chart->axes(Qt::Vertical);
+    if (!axes.isEmpty()) {
+        qobject_cast<QValueAxis *>(axes[0])->setRange(0, maximum);
+        }
+}
+
+
+void Graf::setVisible(int id, bool visible) {
+    PDEBUG << id << visible;
+    QList<QAbstractSeries *> list = m_chart->series();
+    for (int i=0; i<list.size(); i++) {
+        ConstituentSeries *series = qobject_cast<ConstituentSeries *>(m_chart->series()[i]);
+        if (series->internalId() == id) {
+            series->setVisible(visible);
+            return;
+            }
+        }
+}
+
+
+void Graf::drawGraph(const Engine *pEngine)
+{
+    if (m_db != nullptr && !m_db->isOpen()) {
+        m_db->open();
+        }
+
+    pEngine->lock(); 
+    size_t p = pEngine->getNp(); // points
+    QLineSeries *series;
+
+    int id = 0;
+    double inc_x = pEngine->getCapLen() / p;
+    for (auto &sample : pEngine->getMix().getSamples()) {
+        series = qobject_cast<QLineSeries *>(m_chart->series()[id]);
+        double x = 0;
+        QList<double> vlist;
+        QVector<QPointF> plist;
+        for (unsigned int i = 0; i <= p; i++){
+            plist << QPointF(x * 1000.0, sample.getA(0, i));
+            vlist << x;
+            x += inc_x;
+            }
+        series->replace(plist);
+        series->setVisible(sample.visible());
+
+        Dbt::Graf gdata;
+        gdata.time = pEngine->t;
+        gdata.id = id;
+        gdata.values_array = vlist;
+        if (m_db != nullptr) { m_db->save(gdata); }
+
+        id += 1;
+        }
+
+    series = qobject_cast<QLineSeries *>(m_chart->series()[id]);
+    double x = 0;
+    auto hpl = pEngine->getHpl();
+    QVector<QPointF> plist;
+    for (unsigned int i = 0; i <= p; i++) {
+        plist << QPointF(x * 1000.0, (hpl[i] > 0) ? -log(hpl[i]) : 0);
+        x += inc_x;
+        }
+    series->replace(plist);
+    pEngine->unlock(); 
+
+}
+
